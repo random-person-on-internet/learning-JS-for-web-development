7061cf83d9cd7cfb4f99f6c1117f818917f5c445
random-person-on-internet <vedlakkad05@gmail.com>
2024-02-04 15:19:43 +0530

    Arrays and ocjects

---
+++ day 2/json-intro.js
@@ -0,0 +1,15 @@
+const obj = {
+  name: "Ved",
+  age: 18,
+  city: "Ahmedabad",
+};
+
+// obj to JSON string
+const str = JSON.stringify(obj);
+console.log(str);
+
+// JSON string to obj
+const obj2 = JSON.parse(str);
+console.log(obj2);
+
+// bs atluj chhe JSON ma, jlsa kro :)
